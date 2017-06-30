28c7b06e3ebacbd69072f8556146fc6d970f9d31
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-06-30 19:48:11 +0200

    Removes unused dependencies. Changes production settings

--- config.js
+++ config.js
@@ -3,7 +3,6 @@
 module.exports = {
   DB: process.env.DB || 'mongodb://localhost/skoleskyss',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
-  ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'skoleskyss',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
